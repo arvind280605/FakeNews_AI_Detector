369c971c5bf1f976c1510e74a0da771bf12d1289
arvind280605 <arvind.d0028@gmail.com>
2025-11-25 20:00:54 +0530

    Update build script to download models from Drive

--- render_build.sh
+++ render_build.sh
@@ -4,18 +4,25 @@
 mkdir -p model/ai_detector_model
 mkdir -p model/final_model
 
+# ---------------------- INSTALL GDOWN -------------------------
+echo "Checking gdown..."
+if ! command -v gdown &> /dev/null
+then
+    echo "gdown not found. Installing..."
+    pip install --quiet gdown
+fi
+
 # ---------------------- DOWNLOAD AI DETECTOR MODEL ----------------------
 echo "Downloading AI detector model..."
-# Replace the folder URL with the folder ID for AI model
+# Google Drive folder ID for AI detector model
 AI_MODEL_FOLDER_ID="1wkflcPBJ81FSTzeYAPN0JWHfw5ZX-nQH"
 
-# Using gdown to download all files in the folder
-pip install --quiet gdown
+# Download all files in the folder
 gdown --folder https://drive.google.com/drive/folders/$AI_MODEL_FOLDER_ID -O model/ai_detector_model
 
 # ---------------------- DOWNLOAD FAKE MODEL -----------------------------
 echo "Downloading Fake model..."
-# Replace the folder URL with the folder ID for Fake model
+# Google Drive folder ID for Fake model
 FAKE_MODEL_FOLDER_ID="1sIFq6Iuon-qp2W_Gen7RPhzzl-HauVrE"
 
 # Download all files in the folder
