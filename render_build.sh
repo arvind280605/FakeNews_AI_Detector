79126cfb84bec46d0e08257e50220442901c3070
arvind280605 <arvind.d0028@gmail.com>
2025-11-25 20:24:29 +0530

    Update build script to skip LFS and download models from Drive

--- render_build.sh
+++ render_build.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# ---------------------- SKIP GIT LFS DOWNLOAD -------------------------
-git lfs install --skip-smudge
-
-# ---------------------- CLEAN EXISTING MODEL DIRECTORIES -------------------------
+# ---------------------- CLEAN OLD MODELS -------------------------
 rm -rf model/ai_detector_model
 rm -rf model/final_model
 
-# ---------------------- CREATE MODEL DIRECTORIES -------------------------
+# ---------------------- CREATE MODEL DIRECTORIES -----------------
 mkdir -p model/ai_detector_model
 mkdir -p model/final_model
 
-# ---------------------- DOWNLOAD AI DETECTOR MODEL ----------------------
+# ---------------------- DOWNLOAD AI DETECTOR MODEL ----------------
 echo "Downloading AI detector model..."
 AI_MODEL_FOLDER_ID="1wkflcPBJ81FSTzeYAPN0JWHfw5ZX-nQH"
 pip install --quiet gdown
 gdown --folder https://drive.google.com/drive/folders/$AI_MODEL_FOLDER_ID -O model/ai_detector_model
 
-# ---------------------- DOWNLOAD FAKE MODEL -----------------------------
+# ---------------------- DOWNLOAD FAKE MODEL ---------------------
 echo "Downloading Fake model..."
 FAKE_MODEL_FOLDER_ID="1sIFq6Iuon-qp2W_Gen7RPhzzl-HauVrE"
 gdown --folder https://drive.google.com/drive/folders/$FAKE_MODEL_FOLDER_ID -O model/final_model
